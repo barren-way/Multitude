02a5f4bdeac28108c51c053ef7365fa1369ff89b
Barren_Land <xiny_liu@126.com>
2020-07-27 19:18:34 +0800

    update

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    protected Animator anim;
+    protected AudioSource DeathAudio;
+    
+    protected virtual void Start()
+    {
+        anim=GetComponent<Animator>();
+        DeathAudio = GetComponent<AudioSource>();
+    }
+    public void death()
+    {
+        
+        Destroy(gameObject);
+    }
+
+    public void jumpOn()
+    {
+        
+        anim.SetTrigger("death");
+        DeathAudio.Play();
+        
+    }
+
+
+}
