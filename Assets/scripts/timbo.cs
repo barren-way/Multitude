5ddd68de634a6c79a91310e33a409a11ef02d7db
Barren_Land <xiny_liu@126.com>
2020-08-02 14:55:13 +0800

    完成爬梯子

---
+++ Assets/scripts/timbo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timbo : MonoBehaviour
+{
+    public float climbspeed = 2f;
+    private bool inLadder;
+    public Collider2D platf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.tag =="Player")
+        {
+            //向上爬
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                inLadder=true;
+                collider.GetComponent<Rigidbody2D>().gravityScale=0;
+                collider.GetComponent<Rigidbody2D>().velocity =new Vector2(0,climbspeed);
+            }
+            //向下爬
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                inLadder=true;
+                collider.GetComponent<Rigidbody2D>().gravityScale=0;
+                collider.GetComponent<Rigidbody2D>().velocity =new Vector2(0,-climbspeed);
+            }
+            else if(Input.GetKey(KeyCode.Space))
+            {
+                collider.GetComponent<Rigidbody2D>().gravityScale = 1;
+                inLadder = false;
+            }
+            else if(inLadder)
+            {
+                collider.GetComponent<Rigidbody2D>().velocity=new Vector2(0,0);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        collider.GetComponent<Rigidbody2D>().gravityScale=1;
+        inLadder = false;
+    }
+}
