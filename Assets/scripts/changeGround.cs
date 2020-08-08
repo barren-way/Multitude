6cb5e49e7d06df318671e4176f992c12b6584dec
Barren_Land <xiny_liu@126.com>
2020-08-08 15:09:34 +0800

    修改背景及black世界上层地形

---
+++ Assets/scripts/changeGround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ground;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag=="Player")
+        {
+            ground.GetComponent<Collider2D>().isTrigger=false;
+            Destroy(gameObject);
+        }
+    }
+}
