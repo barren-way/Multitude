7209d8ae230e71f00ab3ad038eda329c9f812627
Sunday <1070649673@qq.com>
2020-08-22 12:39:04 +0800

    确定动画播放时机以及UI

---
+++ Assets/scripts/monkey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monkey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool grow;
+    private Animator anim;
+    public GameObject useDialog;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(grow && Input.GetButton("Pull"))
+        {
+            anim.SetBool("grow",true); 
+            useDialog.SetActive(false); 
+        }
+    }
+}
--- Assets/scripts/reagent.cs
+++ Assets/scripts/reagent.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System.Net.Mime;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class reagent : MonoBehaviour
 {
     public GameObject explosionVFXPrefab;
+    public GameObject useDialog;
+    public GameObject Monkey;
+    public int Catalyzer;
+    public Text CatalyzerNum;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +20,19 @@ public class reagent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnCollisionEnter2D(Collision2D collider)
     {
         if(collider.gameObject.tag == "Player")
         {
              Instantiate(explosionVFXPrefab,transform.position,transform.rotation);
-             Destroy(gameObject);
              playermove.PlayOrbAudio();  
-                                    
+             Catalyzer += 1;
+             CatalyzerNum.text = Catalyzer.ToString(); 
+             useDialog.SetActive(true);  
+             Monkey.GetComponent<monkey>().grow = true;
+             Destroy(gameObject);                     
         }
     }
 }
